a32db52ee68247a873d74c9ae351a2647fb84477
Dev Jaja <jajak5242@gmail.com>
2026-01-08 23:45:04 -0500

    feat: implemented MockStrategy.sol

--- smart-contract/contracts/mocks/MockStrategy.sol
+++ smart-contract/contracts/mocks/MockStrategy.sol
@@ -7,28 +7,28 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockStrategy is IStrategy, Ownable {
     address public immutable asset;
-    address public immutable vault;
+    address internal immutable _vault;
 
     uint256 public totalAssetsInvested;
 
     constructor(address _asset, address _vault) Ownable(msg.sender) {
         asset = _asset;
-        vault = _vault;
+        _vault = _vault;
     }
 
     function invest(uint256 _amount) external override {
-        require(msg.sender == vault, "Only vault can invest");
+        require(msg.sender == _vault, "Only vault can invest");
         totalAssetsInvested += _amount;
         IERC20(asset).transferFrom(msg.sender, address(this), _amount);
     }
 
     function withdraw(uint256 _amount) external override returns (uint256) {
-        require(msg.sender == vault, "Only vault can withdraw");
+        require(msg.sender == _vault, "Only vault can withdraw");
         uint256 amountToWithdraw = _amount > totalAssetsInvested
             ? totalAssetsInvested
             : _amount;
         totalAssetsInvested -= amountToWithdraw;
-        IERC20(asset).transfer(vault, amountToWithdraw);
+        IERC20(asset).transfer(_vault, amountToWithdraw);
         return amountToWithdraw;
     }
 
@@ -37,6 +37,6 @@ contract MockStrategy is IStrategy, Ownable {
     }
 
     function vault() external view override returns (address) {
-        return vault;
+        return _vault;
     }
 }
